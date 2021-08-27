738a63883e91924c78a7471345f1664538ed361b
NateBurd99 <burdick.nate@gmail.com>
2021-08-27 16:25:11 -0600

    Initial Application

--- week1/Hello/config/urls.py
+++ week1/Hello/config/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import homePageView
+from pages.views import homePageView
 
 urlpatterns = [
     path('', homePageView),
