18a5dd803ad393da5facd5af44ae8f270fa98201
NateBurd99 <burdick.nate@gmail.com>
2021-09-08 15:19:36 -0600

    added blank page

--- week3/Superhero/config/urls.py
+++ week3/Superhero/config/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from hero.views import HulkView, IronManView , BlackWidowView, HerosView
 
 urlpatterns = [
-    path('hero', HerosView.as_view()),
+    path('', HerosView.as_view()),
     path('hulk', HulkView.as_view()),
     path('ironman', IronManView.as_view()),
     path('blackwidow', BlackWidowView.as_view()),
