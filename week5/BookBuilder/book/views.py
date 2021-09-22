73dae710ebf4b3a2261d786679120de1c695b162
NateBurd99 <burdick.nate@gmail.com>
2021-09-22 15:19:32 -0600

    Lesson13 Themes

--- week5/BookBuilder/book/views.py
+++ week5/BookBuilder/book/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from .models import Book
 
-# Create your views here.
+class BookView(ListView):
+    template_name = 'book_list.html'
+    model = Book
+
+class BookDetailView(DetailView):
+    template_name = 'book_detail.html'
+    model = Book
--- week5/Superhero/hero/views.py
+++ week5/Superhero/hero/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from .models import hero
 
-# Create your views here.
+class HeroView(ListView):
+    template_name = 'hero_list.html'
+    model = hero
+
+class HeroDetailView(DetailView):
+    template_name = 'hero_detail.html'
+    model = hero
