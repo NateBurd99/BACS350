ff6588bb61037ec85e7ec3daa2993c406ffd26fa
NateBurd99 <burdick.nate@gmail.com>
2021-09-29 15:09:12 -0600

    Testadded

--- week6/Superhero/hero/tests.py
+++ week6/Superhero/hero/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import hero
+
+class HeroTest(TestCase):
+
+    def test_django(self):
+        self.assertTrue
+    
+    def test_num_hero(self):
+        self.assertEqual(len(hero.objects.all()), 0)
+
+    def test_add_book(self):
+        hero.objects.create(name='TestMan', description = 'Im a test dummy')
+        self.assertEqual(len(hero.objects.all(),1))
 
 # Create your tests here.
